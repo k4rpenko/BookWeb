324657032161c40a829dae79da723e494020cd3e
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-08-28 23:52:04 +0300

    Redis

--- Server/RedisDAL/RedisConfigure.cs
+++ Server/RedisDAL/RedisConfigure.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace RedisDAL
 {
-    public class RedisConfigure 
+    public class RedisConfigure
     {
         private readonly ConnectionMultiplexer redis;
         private readonly IDatabase _db;
@@ -11,53 +11,10 @@ namespace RedisDAL
         public RedisConfigure(IConfiguration configuration)
         {
             string connectionString = configuration.GetSection("Redis:ConnectionString").Value;
-
-
             redis = ConnectionMultiplexer.Connect(connectionString);
             _db = redis.GetDatabase();
         }
 
         public IDatabase GetDatabase() => _db;
-
-
-        public class Limiting
-        {
-            private static readonly TimeSpan RequestWindow = TimeSpan.FromMinutes(5);
-            private static readonly TimeSpan BlockDuration = TimeSpan.FromMinutes(30);
-            private static readonly int MaxRequests = 5;
-            private readonly IDatabase db;
-
-            public Limiting(IDatabase _db) { db = _db; }
-
-            bool AuthRedisUser(string ip)
-            {
-                var requestKey = $"requests:{ip}";
-
-                if (!db.KeyExists(requestKey))
-                {
-                    db.HashSet(requestKey, new HashEntry[]
-                    {
-                        new HashEntry("Request", 0),
-                        new HashEntry("Blocked", "0")
-                    });
-                    return true;
-                }
-                else
-                {
-                    if(db.HashGet(requestKey, "Blocked").ToString() == "0")
-                    {
-                        db.HashIncrement(requestKey, "Request", 1);
-                    }
-                    else
-                    {
-                        if (DateTime.UtcNow < DateTime.Parse(db.HashGet(requestKey, "Blocked").ToString()));
-                        {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-            }
-        }
     }
 }
---
+++ Server/RedisDAL/RedisLimiting.cs
@@ -0,0 +1,48 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDAL
+{
+    class RedisLimiting
+    {
+        private static readonly TimeSpan RequestWindow = TimeSpan.FromMinutes(5);
+        private static readonly TimeSpan BlockDuration = TimeSpan.FromMinutes(30);
+        private static readonly int MaxRequests = 5;
+        private readonly IDatabase db;
+        public RedisLimiting(IDatabase _db) { db = _db; }
+
+        bool AuthRedisUser(string ip)
+        {
+            var requestKey = $"requests:{ip}";
+
+            if (!db.KeyExists(requestKey))
+            {
+                db.HashSet(requestKey, new HashEntry[]
+                {
+                        new HashEntry("Request", 0),
+                        new HashEntry("Blocked", "0")
+                });
+                return true;
+            }
+            else
+            {
+                if (db.HashGet(requestKey, "Blocked").ToString() == "0")
+                {
+                    db.HashIncrement(requestKey, "Request", 1);
+                }
+                else
+                {
+                    if (DateTime.UtcNow < DateTime.Parse(db.HashGet(requestKey, "Blocked").ToString())) ;
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
